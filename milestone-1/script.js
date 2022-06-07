8df0c3fb1a87c8a5502515a72d78ff20c59d1d02
azzurragraziano <azzurragraziano02@email.com>
2022-06-07 15:57:56 +0200

    milestone 1

--- milestone-1/script.js
+++ milestone-1/script.js
@@ -1,3 +1,27 @@
-/* Per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
+/* Per i multipli di 3 stampi Fizz al posto del numero
+e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 Stampare in console. */
+
+// Scriviamo in console tutti i numeri da 1 a 100
+
+for(let i = 1; i <= 100; i++) {
+
+    /* Per i multipli di 3 stampi Fizz al posto del numero
+    e per i multipli di 5 stampi Buzz. */
+    let fizzOrBuzz;
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        fizzOrBuzz = 'FizzBuzz';
+    } else if (i % 3 === 0) {
+        fizzOrBuzz = 'Fizz';
+    } else if (i % 5 === 0) {
+        fizzOrBuzz = 'Buzz';
+    } else {
+        fizzOrBuzz = i;
+    }
+
+    console.log(fizzOrBuzz);
+ 
+
+}
